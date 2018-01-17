763c8ae9e7cbd042ed176d324f031008855214fd
Sam Marshall <samuel.m.marshall.12@dartmouth.edu>
2018-01-17 13:35:18 -0500

    Add util to rm random missing values

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,21 @@
 # utilities file for package
+
+# make df into returns -------------------------------
 make_ret <- function( df ) {
 
-  # make df into returns
   df %<>% mutate_if(is.numeric, funs(. / lag(.) ) )
 
   df <- df[2:nrow(df),]
   return(df)
-
 }
 
+# not in function ----------------------
 '%!in%' <- function(x,y)!('%in%'(x,y))
+
+# fill in random days that are unpopulated with previous close -----------------
+fill_nm <- function( df ) {
+  df %<>% mutate_if(
+    is.numeric, funs( ifelse( is.na(lag(.)) == FALSE & is.na(.) == TRUE,
+                                               lag(.), .)))
+}
 
