018753df9eebadd889bc591ee1624704baadce61
Sam Marshall <samuel.m.marshall.12@dartmouth.edu>
2018-03-02 14:42:23 -0500

    Make returns cumsum

--- R/ret_type.R
+++ R/ret_type.R
@@ -2,13 +2,14 @@
 #'
 #' This function takes a data frame of stock prices and a date variable created
 #'   by get_close and creates a new data frame with returns for different
-#'   durations. The default options are daily, weekly and monthly. By
+#'   durations. The default options are daily, weekly, monthly, and total. By
 #'   specifying a number for per.len, the returns will be for a period of that
 #'   many days.
 #' @param df data frame
-#' @param per.len character. The length of the period. E.g. weekly, monthly, daily, other
+#' @param per.len character. The length of the period. E.g. weekly, monthly, daily, total
 #' @param per.st the start of the period. Default to 1. For weekdays, 1 corresponds to Sunday.
 #' @param ann If TRUE, the returns be annualized. Default to FALSE
+#' @param quietly FALSE, If TRUE, then it will not print the period length
 #' @keywords return variance
 #' @import magrittr
 #' @import dplyr
@@ -16,7 +17,7 @@
 #' @examples
 #' ret_type()
 
-ret_type <- function( df, per.len, per.st = 1, ann = FALSE ) {
+ret_type <- function( df, per.len, per.st = 1, ann = FALSE, queitly = FALSE ) {
 
   if (is.numeric(per.len) == FALSE) {per.len <- tolower(per.len)}
 
@@ -25,7 +26,6 @@ ret_type <- function( df, per.len, per.st = 1, ann = FALSE ) {
   # daily returns ------------------------------------------
   if (per.len == "daily") {
     out.df <- ret.df
-    print("Returns are daily")
   }
 
   # weekly returns ------------------------------------------
@@ -43,25 +43,49 @@ ret_type <- function( df, per.len, per.st = 1, ann = FALSE ) {
     if (out.df$N_days[1] < 5) {out.df <- out.df[2:nrow(out.df),]}
     if (out.df$N_days[nrow(out.df)] < 5) {out.df <- out.df[1:(nrow(out.df)-1),]}
 
-    # remove temp vars and display note about return
-    print("Returns are weekly")
+    # remove temp vars
     out.df %<>% ungroup() %>% select(-week.per, -N_days)
   }
 
-  # monthly returns ------------------------------------------
+  # monthly returns -----------------------------------------------------------
   if (per.len == "monthly") {
-    temp.df <- ret.df %>%
+    out.df <- ret.df %>%
       mutate(ym_date = as.yearmon( date_id) ) %>%
-      group_by( ym_date )
-
-    out.df <- merge(
-      temp.df %>% summarise( N_days = n()),
-      temp.df %>% summarise_if( is.numeric, funs( (sum(. - 1) * 100) ) ),
-      by = "ym_date" ) %>%
+      group_by( ym_date ) %>%
+      mutate_if(is.numeric, funs(cumprod)) %>%
+      mutate(l.date = max(date_id),
+             N_days = n()) %>%
+      filter( date_id == l.date)  %>%
       filter(N_days > 15) %>%
       ungroup() %>%
-      select( -N_days)
-    print("Retunrs are monthly")
+      select( -N_days, -l.date, -date_id) %>%
+      select(ym_date, everything()) %>%
+      mutate_at(vars(-ym_date), funs((. - 1) * 100))
+
+    # temp.df <- ret.df %>%
+    #   mutate(ym_date = as.yearmon( date_id) ) %>%
+    #   group_by( ym_date )
+    #
+    # out.df <- merge(
+    #   temp.df %>% summarise( N_days = n()),
+    #   temp.df %>% summarise_if( is.numeric, funs( (sum(. - 1) * 100) ) ),
+    #   by = "ym_date" ) %>%
+    #   filter(N_days > 15) %>%
+    #   ungroup() %>%
+    #   select( -N_days)
+
+
+  }
+
+  # total returns -----------------------------------------------------------
+  if (per.len == "total") {
+    dt.start <- min(ret.df$date_id) - days(1)
+    dt.end <- max(ret.df$date_id)
+
+    out.df <- ret.df %>%
+      mutate_if(is.numeric, funs(cumprod)) %>%
+      filter(date_id == dt.end) %>%
+      select(-date_id)
   }
 
   # other frequency returns ------------------------------------------
@@ -78,6 +102,21 @@ ret_type <- function( df, per.len, per.st = 1, ann = FALSE ) {
     # remove temp vars and display note about return
     print(paste0("Returns are for ", per.len, " day periods"))
     out.df %<>% ungroup() %>% select(-period.id, -N_days)
+  }
+
+  # print the type of returns if specified ------------------------------------
+  if (quietly == FALSE) {
+    if(per.len %in% c("daily", "weekly", "monthly")) {
+      print(paste0("Retunrs are ", per.len))
+    }
+
+    if(per.len == "total") {
+        print(pasate0("Total Returns for period ", dt.start, " - ", dt.end))
+    }
+
+    if (is.numeric(per.len) == TRUE) {
+      print(paste0("Returns are for ", per.len, " day periods"))
+    }
   }
 
 
--- R/solve_frontier.R
+++ R/solve_frontier.R
@@ -1,10 +1,14 @@
 #' calculate the efficient frontier
 #'
-#' In more detail ... This function
-#' @param df the dataframe to be manipulated
+#' This function takes a dataframe of stock prices and calculates the mean-
+#'   variance efficient frontier. The period length can be manipulated to
+#'   use an alternate time length for calculating the vcov matrix.
+#' @param df a dataframe to stock prices
+#' @param S_period the period length for the variance matrix. See \code{\link{ret_type}}
 #' @param min_ret the minimum return
 #' @param max_ret the maximum return
 #' @param size_out the number of obs in the returned df
+#' @return a dataframe of expected returns and variances
 #' @keywords finance markowitz bullet
 #' @import magrittr
 #' @import dplyr
@@ -12,29 +16,44 @@
 #' @examples
 #' solve_frontier()
 #'
-# function to make the frontier
-solve_frontier <- function( df, min_ret = 1.0001, max_ret = 1.01,
-                            size_out = 100) {
-
-  # need the vcov matrix to calculate the ptf var, the stock weights and N stocks
-  # make the vcov matrix
-  S <- df %>% select_if( is.numeric ) %>% var( )
-
-  # get the number of stocks
-  dim_ptf <- ncol( df ) - 1
+solve_frontier <- function( df, S_period = "monthly", min_ret = 1.0001,
+                         max_ret = 1.5, size_out = 100) {
 
   # sequence of returns for the bullet
   bullet_ret <- seq( from = min_ret, to = max_ret, length.out = size_out )
 
-  #baseline to start the df
-  # calculate the efficient weights
-  eff_weights <- ptf_wts( df, min_ret)
-  pf_var <- as.numeric( t( eff_weights ) %*% S %*% eff_weights )
+  # calculate the A-inv matrix
+  #-----------------------------------
+  # make the vcov matrix
+  S <- ret_type(df, S_period) %>% select_if( is.numeric ) %>% var( )
 
-  bullet.df <- data_frame( E_ret = min_ret, ptf_var = pf_var )
+  # make the vactor of E returns
+  mu <- ret_type(df, "monthly") %>% summarise_if( is.numeric, funs( mean ) ) %>%
+    as.matrix( ) %>% t( )
 
-  for( i in 2: size_out ) {
-    eff_weights <- ptf_wts( df, bullet_ret[ i ] )
+  dim_ptf <- nrow( mu )  # get the number of stocks
+
+  #create the A matrix
+  A <- rbind(
+    cbind( 2 * S, mu,  rep(1, dim_ptf) ),
+    cbind( t( mu ), 0, 0),
+    cbind( t( rep(1, dim_ptf ) ), 0, 0) )
+
+  # get the inverse of A
+  A_inv <- solve( A )
+  #-------------------------------------
+
+  bullet.df <- data.frame()
+
+  for( i in 1: size_out ) {
+    # make the b_0 vector
+    b <- matrix( data = c( rep(0, dim_ptf ), bullet_ret[ i ], 1),
+                 nrow = dim_ptf + 2, ncol = 1)
+
+    #solution vec is then
+    z <- A_inv %*% b
+    eff_weights <- as.matrix( z[1: dim_ptf] )  # subset to only give the ptf weights
+
     pf_var <- as.numeric( t( eff_weights ) %*% S %*% eff_weights )
 
     bullet.df <- bind_rows(
@@ -45,4 +64,3 @@ solve_frontier <- function( df, min_ret = 1.0001, max_ret = 1.01,
   return( bullet.df )
 
 }
-
